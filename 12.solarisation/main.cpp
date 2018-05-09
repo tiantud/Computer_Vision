45f190d3060763793b3f39522ffe95cbdb72bb67
tiantud <tianz0422@gmail.com>
2018-05-09 20:02:31 +0200

    sth strange happend in solarisation

--- 12.solarisation/main.cpp
+++ 12.solarisation/main.cpp
@@ -51,36 +51,71 @@ void on_mouse( int event, int x, int y, int flags, void* param )
                SrcPtInt[nb_points].x=x;
                SrcPtInt[nb_points].y=y;
                if (nb_points)
-               {   // second point in SrcPtInt
+               {
+                  cout << "In the 1.st situation" << endl;
+                  // second point in SrcPtInt
 
                   MapCurveimage512 = 0;
 
                   // read the two extrema points
                   int x1 = SrcPtInt[0].x;
-                      int x2 = SrcPtInt[1].x;
+                  int x2 = SrcPtInt[1].x;
                   int y1 = 511 - SrcPtInt[0].y; // "511 -" cause we want "0" in lower left corner
                   int y2 = 511 - SrcPtInt[1].y;
 
-                  // determine polynomial coefficients of y(x):=a*(x-x0)�-b*x+c
-                  // (want to cheat? see end of file)
+
+                  // determine polynomial coefficients of y(x):=a*(x-x0)^3-b*x+c
+                  float x0,a,b,c,k;
+                  x0 = (float)0.5 * abs(x1+x2);
+                  k = (float)0.75 * pow(x1-x2, 2);
+                  a = (y1 - y2) / (pow(x1 - x0, 3)-pow(x2-x0, 3) - k * (x1 - x2));
+                  b = k * a;
+                  c = y1 - a * (pow(x1 - x0, 3) - k * x1);
+
+                  // output parameters for test
+
+                  cout << "Shoud print my positions now!" << endl;
+                  cout<<"First point:" << x1 << "," << y1 << endl;
+                  cout<<"Second point:" << x2 << "," << y2 << endl;
+                  cout<<"x0::" << x0 << endl;
+                  cout<<"a:" << a << endl;
+                  cout<<"b:" << b << endl;
+                  cout<<"c:" << c << endl;
+                  cout<< endl;
+                  cout<< endl;
 
                   // create the LUT for that polynom and
                   // draw the polynom in the MapCurveimage (pixelwise)
-                      // your code for the polynomial and color transform (instead of the following line)
-                  line(MapCurveimage512, SrcPtInt[0], SrcPtInt[1], CV_RGB(255, 255, 255));	// remove that line example
+                  // your code for the polynomial and color transform (instead of the following line)
+//                  line(MapCurveimage512, SrcPtInt[0], SrcPtInt[1], CV_RGB(255, 255, 255));	// remove that line example
+                  int x,y;
+                  Vec3b color = (255,255,255);
+
+
+                  for(x = 0;x <= 511;x++){
+                      y = a * pow((x-x0), 3) - b * x + c;
+                      y = 511 - y;
+                      if(y >= 0 && y <= 512){
+                          MapCurveimage512.at<Vec3b>(Point(x/3,y)) = color;
+                          cout<<"x,y:"<<x/3<<","<<y<<endl;
+                      }
+                  }
 
                   // use the lookup table (LUT) to map the input image to the result image
                   // use the same LUT for each color channel (or fantasize)
 
-                      // show non-linear mapping curve
+                  // show non-linear mapping curve
                   imshow("GreyCurve", MapCurveimage512);
 
-                      // show the result
-                      imshow( "result image", mapped_result_img );
+                  // show the result
+                  imshow( "result image", mapped_result_img );
 
-                      // mapped_result_img = image.clone();
-                      nb_points = 0;
-               } else nb_points++;
+                  // mapped_result_img = image.clone();
+                  nb_points = 0;
+               } else {
+                   nb_points++;
+                   cout << "In the 2.st situation" << endl;
+               }
                }
         }
         break;
@@ -92,6 +127,8 @@ int main( int argc, char** argv )
 {
     help();
 
+    cout << "hello!" << endl;
+
     char* filename = argc == 3 ? argv[1] : (char*)"../data/fruits.jpg";
     image = imread(filename, 1);
     mapped_result_img = image.clone();
@@ -100,9 +137,9 @@ int main( int argc, char** argv )
     namedWindow( "Fruits!");
     imshow( "Fruits!", mapped_result_img );
 
-   MapCurveimage512.create(512, 512, CV_8U);
-   MapCurveimage512 = 0;
-   imshow("GreyCurve", MapCurveimage512);
+    MapCurveimage512.create(512, 512, CV_8U);
+    MapCurveimage512 = 0;
+    imshow("GreyCurve", MapCurveimage512);
 
     cvSetMouseCallback( "GreyCurve", on_mouse, 0 );
     waitKey();
