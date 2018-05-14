0771b8fd701bd64d68d5b83cbf466b733e89198f
tiantud <tianz0422@gmail.com>
2018-05-14 17:23:40 +0200

    update project configuration

--- 13.orientation_hist/main.cpp
+++ 13.orientation_hist/main.cpp
@@ -11,13 +11,28 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-    Mat image;
+    Mat image, horizGradient, vertiGradient, gradient;
     char* filename = argc == 3 ? argv[1] : (char*)"./images/lines1.png";
-    image = imread(filename, CV_LOAD_IMAGE_COLOR);
+    image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+
+    cout<< "image information:"<< endl;
+    cout<< "   rols: "<< image.rows<< endl;
+    cout<< "   cols: "<< image.cols<< endl;
 
     namedWindow( "Origional image", WINDOW_AUTOSIZE);
     imshow( "Origional image", image );
 
+    Mat row;
+    for(int i = 0; i < image.rows; i++)
+    {
+        // for each rows in image do:
+        row = image.row(i);
+        for(int j = 0; j < image.cols; j++)
+        {
+            // for each pixel in this row do:
+
+        }
+    }
 
 
     waitKey(0);
