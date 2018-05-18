bd7213f6bec4e9775a82921ea9aaed54b453afed
tiantud <tianz0422@gmail.com>
2018-05-18 13:49:57 +0200

    add some commitment

--- 13.orientation_hist/main.cpp
+++ 13.orientation_hist/main.cpp
@@ -7,17 +7,21 @@
 #include <iostream>
 #include <string>
 
-#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES // for PI
 
 using namespace cv;
 using namespace std;
 
+// calculate gradient for every pixel
 int getGrad(int y, int x){
     int result;
 
     if(y == 0 && x == 0){
+        // 500 is a unvalid value,
+        // for (0,0) shoule not record any gradient.
         result = 500;
     } else if (x == 0) {
+        // x == 0 and y != 0 means its a vertical gradient
         result = 90;
     } else {
         result = atan((float)y/(float)x) * 180 / M_PI;
@@ -28,6 +32,7 @@ int getGrad(int y, int x){
 
 int main(int argc, char *argv[])
 {
+    int grad, max = 0;
     Mat image, vertiGradientImage, horizGradientImage, gradientImage;
 
     // overall filter
@@ -60,7 +65,7 @@ int main(int argc, char *argv[])
     image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
 
     // blur image for better result
-    filter2D (image,image,CV_32F,blu);
+//    filter2D (image,image,CV_32F,blu);
 
     // calculate histal and vertical gradient
     filter2D (image,vertiGradientImage,CV_32F,vertiMask);
@@ -71,18 +76,23 @@ int main(int argc, char *argv[])
     filter2D (image,gradientImage,CV_32F,overallMask);
 
     // calculate data in the gradient-mount list
-    int grad, max = 0;
     for(int row = 0; row < image.rows; row++)
     {
         for(int col = 0; col < image.cols; col++)
         {
+            // for each pixel do:
+
+            // calculate gradient
             grad = getGrad((int)vertiGradientImage.at<float>(Point(col,row)), (int)horizGradientImage.at<float>(Point(col,row)));
 
+            // record gradient into gradient-mount list
             if(grad < 400)
             {
+                // if gradient is valid
                 histoList[grad + 89] += 1;
             }
 
+            // get maximun of mount in list, will be used for normalisation of hight
             if(histoList[grad + 89] > max)
             {
                 max = histoList[grad + 89];
@@ -92,7 +102,11 @@ int main(int argc, char *argv[])
 
     // draw histaldiagram with list of data
     for(int col = 0; col < gradientHisto.cols; col++){
+
+        // normalisation of hight
         int h = histoList[col/3] * (gradientHisto.rows-1) / max;
+
+        // draw lines
         line( gradientHisto, Point(col, gradientHisto.rows-1) ,
                              Point(col, gradientHisto.rows-1 - h),
                                Scalar( 255, 0, 0), 1, 8, 0  );
